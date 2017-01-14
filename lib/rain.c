bc8f5d81d3af4f56161f53b9621643fa7fcc3eb1
Philip Dexter <philip.dexter@gmail.com>
2017-01-14 14:24:01 -0500

    Fix length on rain_set_strcpy call

--- lib/rain.c
+++ lib/rain.c
@@ -415,14 +415,14 @@ void rain_get(box *ret, box *table, box *key) {
       char *buf = malloc(2);
       buf[0] = table->data.s[key->data.si];
       buf[1] = 0;
-      rain_set_strcpy(ret, buf, 2);
+      rain_set_strcpy(ret, buf, 1);
       free(buf);
     }
     else if(key->data.si < 0 && key->data.si >= -(table->size)) {
       char *buf = malloc(2);
       buf[0] = table->data.s[table->size + key->data.si];
       buf[1] = 0;
-      rain_set_strcpy(ret, buf, 2);
+      rain_set_strcpy(ret, buf, 1);
       free(buf);
     }
     return;
