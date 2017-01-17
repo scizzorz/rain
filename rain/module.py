a6ed2909f35744280760a9aa20781fdaae8177e9
John Weachock <jweachock@gmail.com>
2017-01-17 00:53:20 -0500

    module: Switch order of file searching

--- rain/module.py
+++ rain/module.py
@@ -204,7 +204,7 @@ class Module(S.Scope):
   # find a source file from a module identifier
   @staticmethod
   def find_file(src):
-    paths = [os.environ['RAINLIB'], '.']
+    paths = ['.', os.environ['RAINLIB']]
 
     for path in paths:
       if os.path.isfile(join(path, src) + '.rn'):
