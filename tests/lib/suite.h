a1055dd94bbba763431ec296cc61551d7e3ca8ad
Philip Dexter <philip.dexter@gmail.com>
2017-01-18 10:59:29 -0500

    tests: Use criterion for a C testing framework

---
+++ tests/lib/suite.h
@@ -0,0 +1,8 @@
+
+/* We use rain as a library in this testing framework, therefore
+   the data which is normally defined in rain files and linked to
+   is not present.
+
+   Here, we mock these variables
+*/
+box rain_args;
