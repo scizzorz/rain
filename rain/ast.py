982178f88c680edf1368613e5d627f3b53eb7807
John Weachock <jweachock@gmail.com>
2017-01-25 22:24:31 -0500

    ast: Start switching to camel dumps

--- rain/ast.py
+++ rain/ast.py
@@ -1,4 +1,8 @@
+import camel
 import fixedint
+
+registry = camel.CamelRegistry()
+machine = camel.Camel([registry])
 
 # Base classes
 
@@ -7,6 +11,17 @@ class metanode(type):
     setattr(cls, func.__name__, func)
     return func
 
+  def __init__(cls, name, bases, attrs):
+    super().__init__(name, bases, attrs)
+    registry.dumper(cls, cls.__tag__, version=cls.__version__)(cls.dump)
+    registry.loader(cls.__tag__, version=cls.__version__)(cls.load)
+
+  def dump(cls, self):
+    return {slot: getattr(self, slot) for slot in cls.__slots__}
+
+  def load(cls, data, version):
+    return cls(*(data[slot] for slot in cls.__slots__))
+
   def __str__(self):
     return self.__name__
 
@@ -14,6 +29,10 @@ class metanode(type):
     return '<{}>'.format(self.__name__)
 
 class node(metaclass=metanode):
+  __tag__ = 'node'
+  __version__ = 1
+  __slots__ = []
+
   def __str__(self):
     return str(type(self))
 
@@ -21,6 +40,10 @@ class node(metaclass=metanode):
     return repr(type(self))
 
 class value_node(node):
+  __tag__ = 'value'
+  __version__ = 1
+  __slots__ = ['value']
+
   def __init__(self, value=None):
     self.value = value
 
@@ -39,6 +62,10 @@ class value_node(node):
 # structure
 
 class program_node(node):
+  __tag__ = 'program'
+  __version__ = 1
+  __slots__ = ['stmts']
+
   def __init__(self, stmts=[]):
     self.stmts = stmts
 
@@ -46,6 +73,10 @@ class program_node(node):
     return '; '.join(str(x) for x in self.stmts)
 
 class block_node(node):
+  __tag__ = 'block'
+  __version__ = 1
+  __slots__ = ['stmts']
+
   def __init__(self, stmts=[]):
     self.stmts = stmts
 
@@ -55,6 +86,10 @@ class block_node(node):
 # statements
 
 class assn_node(node):
+  __tag__ = 'assn'
+  __version__ = 1
+  __slots__ = ['lhs', 'rhs', 'let']
+
   def __init__(self, lhs, rhs, let=False):
     self.lhs = lhs
     self.rhs = rhs
@@ -64,19 +99,35 @@ class assn_node(node):
     return 'assn {!s} = {!s}'.format(self.lhs, self.rhs)
 
 class break_node(node):
+  __tag__ = 'break'
+  __version__ = 1
+  __slots__ = ['cond']
+
   def __init__(self, cond=None):
     self.cond = cond
 
 class catch_node(node):
+  __tag__ = 'catch'
+  __version__ = 1
+  __slots__ = ['name', 'body']
+
   def __init__(self, name, body):
     self.name = name
     self.body = body
 
 class cont_node(node):
+  __tag__ = 'continue'
+  __version__ = 1
+  __slots__ = ['cond']
+
   def __init__(self, cond=None):
     self.cond = cond
 
 class export_node(node):
+  __tag__ = 'export'
+  __version__ = 1
+  __slots__ = ['val', 'name']
+
   def __init__(self, val, name):
     self.val = val
     self.name = name
@@ -85,6 +136,10 @@ class export_node(node):
     return 'export({!s} as {!s})'.format(self.val, self.name)
 
 class if_node(node):
+  __tag__ = 'if'
+  __version__ = 1
+  __slots__ = ['pred', 'body', 'els']
+
   def __init__(self, pred, body, els=None):
     self.pred = pred
     self.body = body
@@ -94,6 +149,10 @@ class if_node(node):
     return 'if {!s} {{ {!s} }} else {{ {!s} }}'.format(self.pred, self.body, self.els)
 
 class loop_node(node):
+  __tag__ = 'loop'
+  __version__ = 1
+  __slots__ = ['body']
+
   def __init__(self, body):
     self.body = body
 
@@ -101,15 +160,19 @@ class loop_node(node):
     return 'loop {{ {!s} }}'.format(self.body)
 
 class pass_node(node):
-  pass
+  __tag__ = 'pass'
 
 class return_node(value_node):
-  pass
+  __tag__ = 'return'
 
 class save_node(value_node):
-  pass
+  __tag__ = 'save'
 
 class until_node(node):
+  __tag__ = 'until'
+  __version__ = 1
+  __slots = ['pred', 'body']
+
   def __init__(self, pred, body):
     self.pred = pred
     self.body = body
@@ -118,6 +181,10 @@ class until_node(node):
     return 'until {!s} {{ {!s} }}'.format(self.pred, self.body)
 
 class while_node(node):
+  __tag__ = 'while'
+  __version__ = 1
+  __slots = ['pred', 'body']
+
   def __init__(self, pred, body):
     self.pred = pred
     self.body = body
@@ -126,6 +193,10 @@ class while_node(node):
     return 'while {!s} {{ {!s} }}'.format(self.pred, self.body)
 
 class for_node(node):
+  __tag__ = 'for'
+  __version__ = 1
+  __slots = ['name', 'func', 'body']
+
   def __init__(self, name, func, body):
     self.name = name
     self.func = func
@@ -135,6 +206,10 @@ class for_node(node):
     return 'for {!s} in {!s} {{ {!s} }}'.format(self.name, self.func, self.body)
 
 class with_node(node):
+  __tag__ = 'with'
+  __version__ = 1
+  __slots = ['expr', 'params', 'body']
+
   def __init__(self, expr, params, body):
     self.expr = expr
     self.params = params
@@ -143,34 +218,48 @@ class with_node(node):
 # expressions
 
 class idx_node(node):
+  __tag__ = 'index'
+  __version__ = 1
+  __slots__ = ['lhs', 'rhs']
+
   def __init__(self, lhs, rhs):
     self.lhs = lhs
     self.rhs = rhs
 
 class name_node(value_node):
-  pass
+  __tag__ = 'name'
 
 class literal_node:
   def hash(self):
     raise Exception("Can't hash {!s}".format(self))
 
 class null_node(node, literal_node):
+  __tag__ = 'null'
+
   def hash(self):
     return 0
 
 class int_node(value_node, literal_node):
+  __tag__ = 'int'
+
   def hash(self):
     return int(fixedint.UInt64(self.value))
 
 class float_node(value_node, literal_node):
+  __tag__ = 'float'
+
   def hash(self):
     return int(ctypes.c_int.from_buffer(ctypes.c_float(1.0)).value)
 
 class bool_node(value_node, literal_node):
+  __tag__ = 'bool'
+
   def hash(self):
     return int(self.value)
 
 class str_node(value_node, literal_node):
+  __tag__ = 'str'
+
   def hash(self):
     val = fixedint.MutableUInt64(0)
     for char in self.value:
@@ -178,10 +267,18 @@ class str_node(value_node, literal_node):
     return int(val)
 
 class table_node(node):
+  __tag__ = 'table'
+  __version__ = 1
+  __slots__ = ['metatable']
+
   def __init__(self, metatable=None):
     self.metatable = metatable
 
 class extern_node(node):
+  __tag__ = 'extern'
+  __version__ = 1
+  __slots__ = ['name']
+
   def __init__(self, name):
     self.name = name
 
@@ -189,6 +286,10 @@ class extern_node(node):
     return 'extern({!s})'.format(self.name)
 
 class import_node(node):
+  __tag__ = 'import'
+  __version__ = 1
+  __slots__ = ['name', 'rename']
+
   def __init__(self, name, rename=None):
     self.name = name
     self.rename = rename
@@ -197,6 +298,10 @@ class import_node(node):
     return 'import({!s})'.format(self.name)
 
 class func_node(node):
+  __tag__ = 'func'
+  __version__ = 1
+  __slots__ = ['params', 'body']
+
   def __init__(self, params, body):
     self.params = params
     self.body = body
@@ -205,6 +310,10 @@ class func_node(node):
     return 'func({!s}) {{ {!s} }}'.format(', '.join(str(x) for x in self.params), self.body)
 
 class call_node(node):
+  __tag__ = 'call'
+  __version__ = 1
+  __slots__ = ['func', 'args', 'catch']
+
   def __init__(self, func, args, catch=False):
     self.func = func
     self.args = args
@@ -214,6 +323,10 @@ class call_node(node):
     return '{!s}({!s})'.format(self.func, ', '.join(str(x) for x in self.args))
 
 class meth_node(node):
+  __tag__ = 'method'
+  __version__ = 1
+  __slots__ = ['lhs', 'rhs', 'args', 'catch']
+
   def __init__(self, lhs, rhs, args, catch=False):
     self.lhs = lhs
     self.rhs = rhs
@@ -224,6 +337,10 @@ class meth_node(node):
     return '{!s}:{!s}({!s})'.format(self.lhs, self.rhs, ', '.join(str(x) for x in self.args))
 
 class bind_node(node):
+  __tag__ = 'binding'
+  __version__ = 1
+  __slots__ = ['lhs', 'rhs']
+
   def __init__(self, lhs, rhs):
     self.lhs = lhs
     self.rhs = rhs
@@ -232,6 +349,10 @@ class bind_node(node):
     return '{!s}:{!s}'.format(self.lhs, self.rhs)
 
 class is_node(node):
+  __tag__ = 'is'
+  __version__ = 1
+  __slots__ = ['lhs', 'typ']
+
   def __init__(self, lhs, typ):
     self.lhs = lhs
     self.typ = typ
@@ -240,6 +361,10 @@ class is_node(node):
     return '{!s} is {!s}'.format(self.lhs, self.typ)
 
 class binary_node(node):
+  __tag__ = 'binary'
+  __version__ = 1
+  __slots__ = ['lhs', 'rhs', 'op']
+
   def __init__(self, lhs, rhs, op):
     self.lhs = lhs
     self.rhs = rhs
@@ -249,6 +374,10 @@ class binary_node(node):
     return '({!s} {!s} {!s})'.format(self.lhs, self.op, self.rhs)
 
 class unary_node(node):
+  __tag__ = 'unary'
+  __version__ = 1
+  __slots__ = ['op', 'val']
+
   def __init__(self, op, val):
     self.op = op
     self.val = val
