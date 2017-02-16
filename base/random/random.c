9e494b55d2574ef327263af6042e6ba4a5aba565
John Weachock <jweachock@gmail.com>
2017-02-15 23:49:34 -0500

    base: Rewrite random module
    Now it can be used as a metatable.

--- base/random/random.c
+++ base/random/random.c
@@ -1,9 +1,8 @@
-#include "rand.h"
+#include "random.h"
 #include <stdlib.h>
 
 void rain_ext_rand(box * ret) {
-  double num_rand = (double)rand()  / (double)RAND_MAX;
-  rain_set_float(ret, num_rand);
+  rain_set_int(ret, rand());
 }
 
 void rain_ext_srand(box * ret, box * seed) {
