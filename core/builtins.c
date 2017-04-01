c6101c894844b7e17b493c985d6c88192ce05fa1
John Weachock <jweachock@gmail.com>
2017-04-01 16:46:05 -0400

    core: Small cleanups in a few places

--- core/builtins.c
+++ core/builtins.c
@@ -96,7 +96,7 @@ void rain_ext_to_str(box *ret, box *val) {
       break;
 
     case ITYP_FUNC:
-      sprintf(rain_to_str_buf, "func 0x%08lx + %08lx", val->data.ui, (unsigned long)(val->env));
+      sprintf(rain_to_str_buf, "func 0x%08lx", val->data.ui);
       rain_set_strcpy(ret, rain_to_str_buf, strlen(rain_to_str_buf));
       break;
 
