12977c8afeb1f20319533d30e531281211d1d6e8
John Weachock <jweachock@gmail.com>
2017-04-03 21:33:03 -0400

    core.env: Damn .gitignore

---
+++ rain/core/env/env.c
@@ -0,0 +1,22 @@
+#include "rain.h"
+#include <stdlib.h>
+#include <string.h>
+
+
+void rain_init_args(int argc, char **argv) {
+  box key;
+  box val;
+
+  for(int i = 0; i < argc; ++i) {
+    rain_set_int(&key, i);
+    rain_set_strcpy(&val, argv[i], strlen(argv[i]));
+    rain_put(rain_args, &key, &val);
+  }
+}
+
+void rain_ext_get_env(box *ret, box *val) {
+  char *env = getenv(val->data.s);
+  if(env) {
+    rain_set_strcpy(ret, env, strlen(env));
+  }
+}
