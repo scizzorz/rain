c6022f47ab14558730f62960b5d38557417ef069
John Weachock <jweachock@gmail.com>
2017-04-08 18:15:38 -0400

    base.platform: Clean up code a bit

--- rain/base/platform/platform.c
+++ rain/base/platform/platform.c
@@ -1,11 +1,30 @@
 #include "rain.h"
 
+#ifdef __linux__
+#define LINUX_BOOL 1
+#define MAC_BOOL 0
+#define PLATFORM_STR "linux"
+
+#elif __APPLE__
+#define LINUX_BOOL 0
+#define MAC_BOOL 1
+#define PLATFORM_STR "mac"
+
+#else
+#define LINUX_BOOL 0
+#define MAC_BOOL 0
+#define PLATFORM_STR "unknown"
+
+#endif
+
 void rain_ext_get_platform(box *ret) {
-  #ifdef __linux__
-    rain_set_str(ret, "linux");
-  #elif __APPLE__
-    rain_set_str(ret, "mac");
-  #else
-    rain_set_str(ret, "unknown");
-  #endif
+  rain_set_str(ret, PLATFORM_STR);
+}
+
+void rain_ext_is_linux(box *ret) {
+  rain_set_bool(ret, LINUX_BOOL);
+}
+
+void rain_ext_is_mac(box *ret) {
+  rain_set_bool(ret, MAC_BOOL);
 }
