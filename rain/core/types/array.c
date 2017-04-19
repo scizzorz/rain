9161ed552be360476333c07eb3162c6ed7f6b054
Philip Dexter <philip.dexter@gmail.com>
2017-04-19 19:47:22 -0400

    core: Check for null in array length

--- rain/core/types/array.c
+++ rain/core/types/array.c
@@ -2,7 +2,14 @@
 
 void rain_ext_array_length(box *ret, box *val) {
   rain_set_int(ret, 0);
+  box elem;
+  box bnull;
+  rain_set_null(&bnull);
   while(rain_has(val, ret)) {
+    rain_get(&elem, val, ret);
+    if (rain_hash_eq(&elem, &bnull)) {
+      break;
+    }
     ret->data.si++;
   }
 }
