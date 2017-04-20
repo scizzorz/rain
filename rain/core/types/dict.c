6f7fe53abef94475f032f485fc6945c3281e5796
John Weachock <jweachock@gmail.com>
2017-04-20 00:34:46 -0400

    core.types.dict: Add values() iterator

--- rain/core/types/dict.c
+++ rain/core/types/dict.c
@@ -1,45 +1,43 @@
 #include "rain.h"
 #include <stdio.h>
 
-void rain_ext_dict_keys_iter(box *ret) {
+item *rain_dict_iter(box *env) {
   box key;
 
   // fetch the box that we're iterating
   rain_set_str(&key, "dict");
-  box *dict = rain_get_ptr(ret, &key);
+  box *dict = rain_get_ptr(env, &key);
 
   // fetch the box that holds our position
   rain_set_str(&key, "cur");
-  box *cur = rain_get_ptr(ret, &key);
-
-  rain_set_null(ret);
+  box *cur = rain_get_ptr(env, &key);
 
   do {
     cur->data.si += 1;
     if(cur->data.si >= dict->data.lpt->max) {
-      return;
+      return NULL;
     }
   } while(dict->data.lpt->items[cur->data.si] == NULL);
 
-  rain_set_box(ret, &(dict->data.lpt->items[cur->data.si]->key));
+  return dict->data.lpt->items[cur->data.si];
 }
 
-void rain_ext_dict_keys(box *ret, box *val) {
-  if(BOX_ISNT(val, TABLE)) {
-    rain_throw(rain_exc_arg_mismatch);
+void rain_ext_dict_keys_iter(box *ret) {
+  item *pair = rain_dict_iter(ret);
+  if(pair == NULL) {
+    rain_set_null(ret);
   }
+  else {
+    rain_set_box(ret, &(pair->key));
+  }
+}
 
-  box key;
-  box cur;
-
-  box *env = rain_new_table();
-  rain_set_func(ret, (void  *)(rain_ext_dict_keys_iter), 0);
-  rain_set_env(ret, env);
-
-  rain_set_str(&key, "cur");
-  rain_set_int(&cur, -1);
-  rain_put(env, &key, &cur);
-
-  rain_set_str(&key, "dict");
-  rain_put(env, &key, val);
+void rain_ext_dict_values_iter(box *ret) {
+  item *pair = rain_dict_iter(ret);
+  if(pair == NULL) {
+    rain_set_null(ret);
+  }
+  else {
+    rain_set_box(ret, &(pair->val));
+  }
 }
