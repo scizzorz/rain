289753c6e63d9fe257ecc7aa3596f844dfd9d280
John Weachock <jweachock@gmail.com>
2017-06-29 11:05:08 -0400

    base.file: Improve readline and add read

--- rain/base/file/file.c
+++ rain/base/file/file.c
@@ -5,8 +5,6 @@
 // TODO this whole thing is just a temporary proof-of-concept
 // it's bad
 
-static char rain_fbuf[4096]; // static buffer
-
 void rain_ext_fopen(box *, box *, box *);
 void rain_ext_fwriteline(box *, box *, box *);
 void rain_ext_freadline(box *, box *);
@@ -37,6 +35,8 @@ void rain_ext_freadline(box *ret, box *this) {
     return;
 
   FILE *fp = (FILE *)this->data.vp;
+  int bufsize = 4096;
+  char *fbuf = GC_malloc(bufsize);
 
   int i = 0;
   char cur;
@@ -50,15 +50,58 @@ void rain_ext_freadline(box *ret, box *this) {
     if(cur == '\n')
       break;
 
-    rain_fbuf[i] = cur; // what's a buffer overflow?
+    if(i >= bufsize) {
+      bufsize *= 2;
+      fbuf = GC_realloc(fbuf, bufsize);
+    }
+
+    fbuf[i] = cur;
     i++;
   }
 
   if(i == 0 && cur == EOF)
     return;
 
-  rain_fbuf[i] = 0;
-  rain_set_strcpy(ret, rain_fbuf, i);
+  fbuf[i] = 0;
+  rain_set_str(ret, fbuf);
+}
+
+void rain_ext_fread(box *ret, box *this, box *length) {
+  if(BOX_ISNT(this, CDATA))
+    return;
+
+  if(BOX_ISNT(length, INT))
+    return;
+
+  FILE *fp = (FILE *)this->data.vp;
+  long size = length->data.si;
+
+  if(size < 0) {
+    long restore = ftell(fp);
+    fseek(fp, 0L, SEEK_END);
+    size = ftell(fp);
+    fseek(fp, restore, SEEK_SET);
+  }
+
+  char *fbuf = GC_malloc(size + 1);
+
+  long i;
+  char cur;
+
+  for(i=0; i<size; i++) {
+    cur = fgetc(fp);
+
+    if(cur == EOF)
+      break;
+
+    fbuf[i] = cur;
+  }
+
+  if(i == 0 && cur == EOF)
+    return;
+
+  fbuf[size] = 0;
+  rain_set_str(ret, fbuf);
 }
 
 void rain_ext_fclose(box *ret, box *this) {
